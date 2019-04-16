162e25ea34f3b7f2cf3d477ee8839e941679f951
Chrisferdynand <46060670+Chrisferdynand@users.noreply.github.com>
2019-04-16 18:50:34 -0400

    Add files via upload

---
+++ asistente.js
@@ -0,0 +1,13 @@
+$(function(){
+    Swal.fire({
+	  position: 'bottom-end',
+	  type: 'warning',
+	  title: 'Attention!',
+	  width: 200,
+	  text: 'Sunday 20th October is the poll day in Bolivia. Thus, we highly recommend to all 			participants not to arrive this day because there will be no public transport.',
+	  showConfirmButton: true,
+	  background: '#FFFFFF',
+	  animation: true,
+	});
+             
+});
